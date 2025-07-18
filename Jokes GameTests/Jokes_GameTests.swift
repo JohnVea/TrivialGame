b0b9d7bc062a0ea53be69187c5c47bf4f3bb2c1b
John Vea <69362544+JohnVea@users.noreply.github.com>
2025-07-18 15:51:12 -0400

    Add files via upload

---
+++ Jokes GameTests/Jokes_GameTests.swift
@@ -0,0 +1,17 @@
+//
+//  Jokes_GameTests.swift
+//  Jokes GameTests
+//
+//  Created by John Vea on 12/2/24.
+//
+
+import Testing
+@testable import Jokes_Game
+
+struct Jokes_GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
